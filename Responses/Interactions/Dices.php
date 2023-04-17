4fda9a07ba0bb72f108c4c64c7e413799a2aa0ee
Rasa <abdurasul2004.02@gmail.com>
2023-04-17 10:50:49 +0500

    cryptographic function random_int() used instead of rand() for dices

--- Responses/Interactions/Dices.php
+++ Responses/Interactions/Dices.php
@@ -2,11 +2,15 @@
 namespace Interactions;
 
 use Core\Methods\AnswerInlineQuery;
+use Exception;
 
 class Dices {
     
     public string $request_query;
 
+    /**
+     * @throws Exception
+     */
     public function __construct($request)
     {
         $this->request_query = $request['inline_query']['query'];
@@ -15,20 +19,9 @@ class Dices {
         {
             $dice_value = (int) substr($this->request_query, (preg_match("/^d\d/", $this->request_query) ) ?  1 :  2);
             if ($dice_value > 1000000000) exit();
-            $result = [
-                [
-                    "type" => "article",
-                    "id" => "0",
-                    "title" => "Бросить $this->request_query",
-                    "description" => "Удачной игры!",
-                    "input_message_content" => [
-                        "message_text" => "<code>d$dice_value</code> <b>результат:</b> <code>" . strval(rand(1, $dice_value)) . "</code>",
-                        "parse_mode" => "HTML"
-                    ]
-                ]
-            ];
+            $message_text = "<code>d$dice_value</code> <b>результат:</b> <code>" . random_int(1, $dice_value) . "</code>";
         }
-        elseif (preg_match("/^([1-9]|[1-9][0-9])(d|к)\d/", $this->request_query))
+        else
         {
             $less_then_ten = preg_match("/^[1-9](d|к)\d/", $this->request_query);
             $is_type_latin = preg_match("/d\d/", $this->request_query);
@@ -47,9 +40,9 @@ class Dices {
             if ($less_then_ten) {
                 $view = "[";
                 for ($i = 0; $i < $amount_of_dices; $i++) {
-                    $temp = rand(1, $dice_value);
+                    $temp = random_int(1, $dice_value);
                     $sum += $temp;
-                    $view .= (string) $temp;
+                    $view .= $temp;
                     if ($i < $amount_of_dices - 1) $view .=  ",";
                 }
                 $view .= "]";
@@ -60,7 +53,7 @@ class Dices {
                 for ($j = 0; $j < $number_of_tuples; $j++) { 
                     $view .= "[";
                     for ($i=0; $i < 10; $i++) {
-                        $temp = rand(1, $dice_value); 
+                        $temp = random_int(1, $dice_value);
                         $sum += $temp;
                         $view .= "$temp";
                         if($i != 9) $view .= ",";
@@ -71,7 +64,7 @@ class Dices {
                 {
                     $view .= "[";
                     for ($i=0; $i < $amount_of_dices % 10; $i++) {
-                        $temp = rand(1, $dice_value);
+                        $temp = random_int(1, $dice_value);
                         $sum += $temp;
                         $view .= "$temp";
                         if($i != $amount_of_dices % 10 - 1) $view .= ",";
@@ -83,28 +76,25 @@ class Dices {
             $message_text = ($amount_of_dices == 1) 
                 ? "<code>d$dice_value</code> <b>результат: $sum</b>"
                 : "<code>$amount_of_dices" . "d$dice_value</code> <b>результат: $sum</b> <code>$view</code>";
-
-            $result = [
-                [
-                    "type" => "article",
-                    "id" => "0",
-                    "title" => "Бросить " . "$this->request_query",
-                    "description" => "Удачной игры!",
-                    "input_message_content" => [
-                        "message_text" => $message_text,
-                        "parse_mode" => "HTML"
-                    ]
-                ]
-            ];
         }
 
-        $response = new AnswerInlineQuery;
-        $response->inline_query_id($request['inline_query']['id'])
+        $result = [
+            [
+                "type" => "article",
+                "id" => "0",
+                "title" => "Бросить $this->request_query",
+                "description" => "Удачной игры!",
+                "input_message_content" => [
+                    "message_text" => $message_text,
+                    "parse_mode" => "HTML"
+                ]
+            ]
+        ];
+
+        (new AnswerInlineQuery)->inline_query_id($request['inline_query']['id'])
             ->results($result)
             ->cache_time(1)
             ->is_personal(true)
             ->send(false, false);
     }
 }
-
-?>
