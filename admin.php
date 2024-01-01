e6d3c36c9711fb954473fc8a7c7d9d4b87085d2c
rasa <abdurasul2004.02@gmail.com>
2024-01-01 18:56:28 +0500

    unnecessary files have been deleted

--- admin.php
+++ admin.php
@@ -1,6 +1,10 @@
 <?php
 
+require_once('./vendor/autoload.php');
+
+use Core\Database\Database;
 use Database\models\Chat;
+use Dotenv\Dotenv;
 use eftec\bladeone\BladeOne;
 
 $views = __DIR__ . '/admin/views';
@@ -8,10 +12,13 @@ $cache = __DIR__ . '/admin/cache';
 
 $blade = new BladeOne($views, $cache);
 
+Dotenv::createUnsafeImmutable(__DIR__)->load();
+new Database;
+
 echo $blade->setView('index')
     ->share([
-        "users_count"=> Chat::count(),
-        "superusers_count"=> Chat::where('rights', '>', '0')->count(),
-        "chats" => Chat::orderBy('rights', 'desc')->orderBy('id')->get()
+        "users_count"=> Chat::query()->count(),
+        "superusers_count"=> Chat::query()->where('rights', '>', '0')->count(),
+        "chats" => Chat::query()->orderBy('rights', 'desc')->orderBy('id')->get()
     ])
     ->run();
